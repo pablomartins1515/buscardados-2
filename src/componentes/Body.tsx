cc5e9dd250f5093dfe72a7e63aa8aef7fc878832
Pablo Martins <pablomartins1515@hotmail.com>
2022-06-29 20:43:41 -0300

    Add files via upload

---
+++ src/componentes/Body.tsx
@@ -0,0 +1,34 @@
+import fotoCarro from '../assets/foto-carro.png'
+import { Buttonplaceh } from './Buttonplaceh'
+
+export function Body() {
+
+    return (
+
+        <body className=' flex grid grid-flow-col'>
+            <div >
+                <h1 className=" py-16 flex text-center justify-center font-black text-5xl text-blue-900">
+                    Vai comprar um veículo?
+                </h1>
+
+                <h2 className="text-center justify-center text-xl">
+                    Saber seu histórico nunca foi tão fácil! <br />
+                    Consulte pela placa e <br /> receba diversas informações. 😉
+                </h2>
+
+                <Buttonplaceh />
+            </div>
+
+            <div className='flex justify-center'>
+                <div className="max-w-2xl right-4 ">
+                    < img src="https://images2.imgbox.com/2a/27/QsyDDeev_o.png" alt=" Foto carro placa buscar dados consultar dados pela placa" />
+                </div>
+            </div >
+        </body>
+    )
+}
+
+
+// className="px-10 max-w-3xl  right-4"
+
+
